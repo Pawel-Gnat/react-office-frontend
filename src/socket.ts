35b81a63b6fd8b8cec477a3f060faa3c6f4cc33a
Pawel Gnat <pawel_gnat@o2.pl>
2024-07-04 20:45:23 +0200

    fix: remove credentials

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,4 @@ import { io } from 'socket.io-client'
 
 import { DB_URL } from './utils/database'
 
-export const socket = io(DB_URL, {
-	withCredentials: true,
-})
+export const socket = io(DB_URL)
